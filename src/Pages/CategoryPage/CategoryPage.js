15e33274d6061e5e5a5887d3a9a9a3649bd9f4f4
aroy15 <anjonroy57@gmail.com>
2022-11-26 02:43:55 +0600

    category card, modal updated

--- src/Pages/CategoryPage/CategoryPage.js
+++ src/Pages/CategoryPage/CategoryPage.js
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { FaRegTimesCircle } from 'react-icons/fa';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
 import Loading from '../Shared/Loading/Loading';
 import CategoryBanner from './CategoryBanner';
 import CategorySingleCard from './CategorySingleCard';
 
 const CategoryPage = () => {
-    const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(true);
+    const [productData, setProductData] = useState(null);
+    const { user } = useContext(AuthContext);
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
 
     const laptopItems = useLoaderData();
 
+    const handleBooking = data => {
 
-
-    // if (laptopItems.length) {
-    //     setLoading(false)
-    // }
-
-    // if (loading) {
-    //     return <div className="min-h-screen bg-base-200 flex justify-center items-center">
-    //         <Loading></Loading>
-    //     </div>
-    // }
+    }
 
     return (
         <>
@@ -31,21 +31,50 @@ const CategoryPage = () => {
                             laptopItems.map(laptopItem => <CategorySingleCard
                                 key={laptopItem._id}
                                 laptopItem={laptopItem}
+                                setProductData={setProductData}
                             ></CategorySingleCard>)
                         }
                     </div>
-                    <input type="checkbox" id="booking-modal" className="modal-toggle" />
-                    <div className="modal">
-                        <div className="modal-box">
-                            <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
-                            <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                            <div className="modal-action">
-                                <label htmlFor="booking-modal" className="btn">Yay!</label>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </section>
+
+            {/* Booking Modal */}
+
+            <input type="checkbox" id="booking-modal" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box relative">
+                    <label htmlFor="booking-modal" className='absolute top-3 right-3 text-xl text-red-600 cursor-pointer'><FaRegTimesCircle /></label>
+                    <h3 className="font-bold text-2xl text-center mb-5 pt-1">Fill the Form to Book the Product</h3>
+                    <form onSubmit={handleSubmit(handleBooking)} className='flex flex-col gap-3'>
+                        <div>
+                            <p className="italic">User Name:</p>
+                            <input name='name' type="text" defaultValue={user?.displayName} placeholder="name" className="input input-bordered py-2 h-auto w-full rounded-md" readOnly />
+                        </div>
+                        <div>
+                            <p className="italic">Email Address:</p>
+                            <input name='email' type="email" defaultValue={user?.email} placeholder="Email" className="input input-bordered py-2 h-auto w-full rounded-md" readOnly />
+                        </div>
+                        <div>
+                            <p className="italic">Product Name:</p>
+                            <input name='productName' type="text" defaultValue={productData?.name} placeholder="Product Name" className="input input-bordered py-2 h-auto w-full rounded-md" readOnly />
+                        </div>
+                        <div>
+                            <p className="italic">Product Price ($):</p>
+                            <input name='productPrice' type="number" defaultValue={productData?.resalePrice} placeholder="Product Price" className="input input-bordered py-2 h-auto w-full rounded-md" readOnly />
+                        </div>
+                        <div>
+                            <p className="italic">Your Phone Number:</p>
+                            <input type="tel" placeholder="Enter Your Phone Number" className="input input-bordered py-2 h-auto w-full rounded-md" required />
+                        </div>
+                        <div>
+                            <p className="italic">Your Meeting Location:</p>
+                            <input type="text" placeholder="Enter Your Meeting Location" className="input input-bordered py-2 h-auto w-full rounded-md" required />
+                        </div>
+                        <button type='submit'><label htmlFor="booking-modal" className="btn bg-secondary hover:bg-green-600 border-0 rounded-md capitalize text-white w-full text-lg">Submit Booking</label></button>
+                    </form>
+
+                </div>
+            </div>
         </>
     );
 };
--- src/Pages/CategoryPage/CategorySingleCard.js
+++ src/Pages/CategoryPage/CategorySingleCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { FaCheckCircle, FaPhoneAlt, FaMapMarkerAlt, FaPen } from 'react-icons/fa';
+import { FaCheckCircle, FaPhoneAlt, FaMapMarkerAlt } from 'react-icons/fa';
 
-const CategorySingleCard = ({ laptopItem }) => {
+const CategorySingleCard = ({ laptopItem, setProductData }) => {
     const { name, category, categoryImage, image, location, yearsOfUse, resalePrice, originalPrice, postedTime, seller, verified, condition, mobile, description } = laptopItem;
     return (
         <div className="flex flex-col md:flex-row md:flex-nowrap gap-5 rounded-xl shadow-lg bg-white overflow-hidden">
@@ -18,13 +18,13 @@ const CategorySingleCard = ({ laptopItem }) => {
                     </div>
                     <div className='font-semibold flex flex-col gap-2 w-full lg:w-1/2 sm:text-left text-center items-center sm:items-start'>
                         <p className='flex gap-2 items-center'> Seller: {verified && <FaCheckCircle title='Verified' className='text-green-600' />} <span className='italic'>{seller}</span></p>
-                        <p className='flex gap-2 items-center'> <FaPhoneAlt className='text-secondary' /> {mobile}</p>
+                        <a className='flex gap-2 items-center' href={`tel:${mobile}`}> <FaPhoneAlt className='text-secondary' /> {mobile}</a>
                         <p className='flex gap-2 items-center'> <FaMapMarkerAlt className='text-secondary' /> {location}</p>
                     </div>
                 </div>
                 <div className="flex justify-between items-center border-t border-base-200 w-full pt-1 gap-3">
                     <span className='text-secondary font-semibold text-sm italic flex flex-wrap'><span className='pr-1'>Posted on:</span> {postedTime}</span>
-                    <label htmlFor="booking-modal" className="btn h-auto min-h-0 bg-secondary hover:bg-green-600 border-0 rounded capitalize py-3 text-white flex gap-2"><FaPen/> Book Now</label>
+                    <label onClick={()=>setProductData(laptopItem)} htmlFor="booking-modal" className="btn h-auto min-h-0 bg-secondary hover:bg-green-600 border-0 rounded capitalize py-3 text-white">Book Now</label>
                 </div>
             </div>
         </div>
