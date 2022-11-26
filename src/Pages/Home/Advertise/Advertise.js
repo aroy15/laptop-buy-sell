8c8d3569ef752a432c094aa79f4308905a2c3be6
aroy15 <anjonroy57@gmail.com>
2022-11-27 00:38:25 +0600

    advertisement section design updated

--- src/Pages/Home/Advertise/Advertise.js
+++ src/Pages/Home/Advertise/Advertise.js
@@ -8,7 +8,7 @@ const Advertise = ({advertisedItems}) => {
         <section className='py-16'>
             <div className="container">
                 <h2 className="text-4xl text-primary font-bold text-center mb-8">Advertisement</h2>
-                <div className="flex flex-col lg:flex-row justify-center gap-6 max-w-5xl w-full mx-auto text-center sm:text-left">
+                <div className="flex flex-col lg:flex-row flex-wrap justify-center max-w-5xl w-full mx-auto text-center sm:text-left">
                     {
                         advertisedItems.map(advertisedItem => <AdvertiseSectionCard
                             key={advertisedItem._id}
