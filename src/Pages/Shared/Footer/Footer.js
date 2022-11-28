f75b917e24e0c02f562a70546602e4008c81330b
aroy15 <anjonroy57@gmail.com>
2022-11-28 19:33:36 +0600

    footer updated

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,38 +1,45 @@
 import React from 'react';
+import { FaFacebook, FaLinkedin } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import logo from '../../../assets/images/logo.png'
 
 const Footer = () => {
     return (
-        <footer className="p-10 bg-center border-t border-gray-300"          
-        >
-            <div className='footer'>
-                <div>
-                    <span className="footer-title">Services</span>
-                    <Link to="/" className="link link-hover">Branding</Link>
-                    <Link to="/" className="link link-hover">Design</Link>
-                    <Link to="/" className="link link-hover">Marketing</Link>
-                    <Link to="/" className="link link-hover">Advertisement</Link>
+
+        <footer className="p-4  sm:p-6 bg-gray-900 border-t border-gray-700">
+            <div className="flex flex-col items-center">
+                <div className="mb-6 md:mb-0">
+                    <Link to='/' className="flex items-center">
+                        <img src={logo} className="mr-3" alt="Website Logo" />
+                    </Link>
                 </div>
-                <div>
-                    <span className="footer-title">Company</span>
-                    <Link to="/" className="link link-hover">About us</Link>
-                    <Link to="/" className="link link-hover">Contact</Link>
-                    <Link to="/" className="link link-hover">Jobs</Link>
-                    <Link to="/" className="link link-hover">Press kit</Link>
-                </div>
-                <div>
-                    <span className="footer-title">Legal</span>
-                    <Link to="/" className="link link-hover">Terms of use</Link>
-                    <Link to="/" className="link link-hover">Privacy policy</Link>
-                    <Link to="/" className="link link-hover">Cookie policy</Link>
+                <ul className="text-gray-400 flex gap-5 pt-8">
+                    <li className="mb-4">
+                        <Link to="/blogs" className="hover:underline">Blogs</Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard" className="hover:underline">Dashboard</Link>
+                    </li>
+                </ul>
+                
+                <div className="flex mt-4 space-x-6 justify-center">
+                    <a target='_blank' href="https://web.facebook.com/Anjon.Roy.Developer/" className="text-gray-500 hover:text-white text-xl">
+                        <FaFacebook />
+                        <span className="sr-only">Facebook page</span>
+                    </a>
+                    <a target='_blank' href="https://www.linkedin.com/in/anjon-roy" className="text-gray-500 hover:text-white text-xl">
+                        <FaLinkedin />
+                        <span className="sr-only">LinkedIn page</span>
+                    </a>
                 </div>
             </div>
-
-            <div className='text-center py-6 mt-10 border-y border-gray-300'>
-                <p>Copyright © 2022 - All right reserved by Laptop Buy Sell Ltd</p>
+            <hr className="my-6  sm:mx-auto border-gray-700 lg:my-8" />
+            <div className="flex justify-center">
+                <span className="text-sm sm:text-center text-gray-400">© 2022 <a href="/" className="hover:underline">Laptop Buy Sell</a>. All Rights Reserved.
+                </span>
             </div>
         </footer>
-    );
+    )
 };
 
 export default Footer;
